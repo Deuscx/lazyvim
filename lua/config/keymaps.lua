7c5ecae1d6b14cb06b62d0f0f73bfa9a587c7ebe
deuscx <1475453714@qq.com>
2023-11-19 05:51:14 +0800

    chore: add diagnostic shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,44 +5,54 @@
 local Util = require("lazyvim.util")
 
 local function map(mode, lhs, rhs, opts)
-    local keys = require("lazy.core.handler").handlers.keys
-    ---@cast keys LazyKeysHandler
-    -- do not create the keymap if a lazy keys handler exists
-    if not keys.active[keys.parse({
-        lhs,
-        mode = mode
-    }).id] then
-        opts = opts or {}
-        opts.silent = opts.silent ~= false
-        if opts.remap and not vim.g.vscode then
-            opts.remap = nil
-        end
-        vim.keymap.set(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({
+    lhs,
+    mode = mode,
+  }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
     end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
 end
 
 if vim.g.vscode then
-    map("", "<Space>", "<Nop>", opts)
-    -- map("", "/", "<Nop>", opts)
-    -- Vim extension
+  local vscode = require('vscode-neovim')
 
-    -- VSCode KeyMap
-    map("n", "<Leader>o", "<Cmd>call VSCodeNotify('workbench.action.closeOtherEditors')<CR>")
-    map("n", "gr", "<Cmd>call VSCodeNotify('editor.action.goToReferences')<CR>")
+  map("", "<Space>", "<Nop>", opts)
+  -- disable move paragraph
+  map("", "(", "<Nop>", opts)
+  -- map("", "/", "<Nop>", opts)
+  -- Vim extension
 
-    map("n", "g[", "<Cmd>call VSCodeNotify('editor.action.marker.prevInFiles')<CR>")
-    map("n", "g]", "<Cmd>call VSCodeNotify('editor.action.marker.nextInFiles')<CR>")
-    -- Copy in normal mode. when https://github.com/vscode-neovim/vscode-neovim/issues/1266 fixed. Remove it and use `y` to copy
-    map("n", "<C-c>", "<Cmd>call VSCodeNotify('editor.action.clipboardCopyAction')<CR>")
+  -- VSCode KeyMap
+  map("n", "<Leader>o", "<Cmd>call VSCodeNotify('workbench.action.closeOtherEditors')<CR>")
+  map("n", "gr", "<Cmd>call VSCodeNotify('editor.action.goToReferences')<CR>")
 
-    map("n", "za", "<Cmd>call VSCodeNotify('editor.toggleFold')<CR>")
+  -- Copy in normal mode. when https://github.com/vscode-neovim/vscode-neovim/issues/1266 fixed. Remove it and use `y` to copy
+  map("n", "<C-c>", "<Cmd>call VSCodeNotify('editor.action.clipboardCopyAction')<CR>")
 
-    -- map({"n", "v"}, "zr", "<Cmd>call VSCodeNotify('editor.action.refactor')<CR>")
-    -- VsCode Extension
-    map("n", "<Leader>i", "<Cmd>call VSCodeNotify('extension.toggleBool')<CR>")
+  map("n", "za", "<Cmd>call VSCodeNotify('editor.toggleFold')<CR>")
+
+  -- map({"n", "v"}, "zr", "<Cmd>call VSCodeNotify('editor.action.refactor')<CR>")
+  -- VsCode Extension
+  map("n", "<Leader>i", "<Cmd>call VSCodeNotify('extension.toggleBool')<CR>")
+  
+  map("n", "<C-j>", function ()
+    vscode.action('editor.action.marker.next')
+  end)
 else
-    -- ordinary Neovim
-    map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+  -- ordinary Neovim
+  map("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+  map("n", "<C-j>", function()
+    vim.diagnostic.goto_next()
+  end)
 end
 
 -- Select All
